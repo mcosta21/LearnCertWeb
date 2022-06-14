17214ae1c36190d57cf8b3923cd40e158fdf2416
mcosta21 <marcioc424@gmail.com>
2022-06-14 20:34:32 -0300

    Adding absolute paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      src: path.resolve('src/'),
+    },
+  }
 })
